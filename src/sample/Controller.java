b07036ca3c59c5d57588e14e2119c12aef982975
stenod <brummelbummel@hotmail.de>
2017-04-26 17:10:18 +0200

    implemented relation and fixes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -16,32 +16,45 @@ public class Controller {
     public Label label;
     public Label label1;
     public LineChart chart;
+    public Label label11;
+    public LineChart chartRelation;
 
     private ArrayList<Double> x = new ArrayList<Double>();
     private int count = 0;
     private XYChart.Series series = new XYChart.Series();
+    private XYChart.Series series1 = new XYChart.Series();
 
     public void calculateX() {
         double mean = 0;
         Random random = new Random();
         int i = random.nextInt(91) + 10;
 
-        x.add(Math.log(2) / Math.log(circle2.getRadius()/circle.getRadius()));
+        double flaecheGeschaetzt = Math.PI * Math.pow(circle.getRadius(), 2.0);
+        double flaecheReal = Math.PI * Math.pow(circle2.getRadius(), 2.0);
+
+
+        x.add(Math.log(0.5) / Math.log(flaecheReal/flaecheGeschaetzt));
 
         for (double value: x) {
             mean += value;
         }
-        label1.setText("X = " + mean/ x.size());
+        //label.setText("reale Fläche: " + flaecheReal + " Fläche: " + flaecheGeschaetzt);
+        label1.setText("gewähltes X: " + mean/ x.size());
+        label11.setText("gewähltes Verhältnis: " + flaecheReal/flaecheGeschaetzt);
 
         count++;
 
-
         series.getData().add(new XYChart.Data(String.valueOf(count), mean/x.size()));
+
+        series1.getData().add(new XYChart.Data(String.valueOf(count), flaecheReal/flaecheGeschaetzt));
+
         //chart.getData().add(series);
         circle2.setRadius(i);
 
         if (!chart.getData().contains(series)) {
             chart.getData().add(series);
+            chartRelation.getData().add(series1);
+
         }
     }
 }
